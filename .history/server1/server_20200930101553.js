46b9b9892cb7402217ebfa12a7d1aaeb436fc862
Wellington  Carvalho de Oliveira <wellingtondevops@gmail.com>
2020-09-30 15:33:22 -0300

    Monitor de sevidores sms sem history

--- .history/server1/server_20200930101553.js
+++
@@ -1,6 +0,0 @@
-const http = require("http")
-
-http.createServer((req,res)=>{
-    res.write("Server1")
-    res.end()
-}).listen(4001)
--- .history/server2/server_20200930101613.js
+++
@@ -1,6 +0,0 @@
-const http = require("http")
-
-http.createServer((req,res)=>{
-    res.write("Server2")
-    res.end()
-}).listen(4002)
